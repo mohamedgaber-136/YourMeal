1d363e4551493220f3ddf395d63c4fb9ec2392af
mohamedgaber-136 <mohamedd.gaberr.13696@gmail.com>
2023-11-07 14:48:36 +0200

    Updates

---
+++ static/js/126.70896591.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkyourmeal=self.webpackChunkyourmeal||[]).push([[126],{9126:function(t,c,l){l.d(c,{kRm:function(){return u}});var r=l(9983);function u(t){return(0,r.w_)({tag:"svg",attr:{fill:"currentColor",viewBox:"0 0 16 16"},child:[{tag:"path",attr:{d:"M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"}}]})(t)}}}]);
+//# sourceMappingURL=126.70896591.chunk.js.map
